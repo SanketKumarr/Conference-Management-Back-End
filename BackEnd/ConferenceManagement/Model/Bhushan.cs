e80f131c68c71c85acec3a5482af9deb5f340765
Bhushan sahu <103677912+bhushansahu@users.noreply.github.com>
2023-07-27 11:20:56 +0530

    update

---
+++ BackEnd/ConferenceManagement/Model/Bhushan.cs
@@ -0,0 +1,10 @@
+namespace ConferenceManagement.Model
+{
+    public class Bhushan
+    {
+        public string Name  { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        
+    }
+}
