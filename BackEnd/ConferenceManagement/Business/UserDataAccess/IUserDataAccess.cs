90a7cea3cb88bcfa2ab115aadf7b325339e4588c
Ashish Sahu <ashsahu9713@gmail.com>
2023-07-27 13:09:37 +0530

    Update

---
+++ BackEnd/ConferenceManagement/Business/UserDataAccess/IUserDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using ConferenceManagement.Infrastructure.Commands.AdminCommands;
+using ConferenceManagement.Infrastructure.Commands.UserCommands;
+using ConferenceManagement.Model;
+
+namespace ConferenceManagement.Business.UserDataAccess
+{
+    public interface IUserDataAccess
+    {
+        Task<bool> BookRoom(BookRoomCommand request);
+        Task<User> GetUserByEmail(string email);
+        Task<User> LoginUser(LoginUserCommand request);
+        Task<bool> AddUser(User user);
+        Task<List<ConferenceRoom>> DisplayAllRoom();
+       
+        Task<User> GetUserById(int user_Id);
+        
+        Task<bool> UpdateUser(User user);
+    }
+}
---
+++ BackEnd/ConferenceManagement/Handlers/AdminHandlers/AddRoomCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using ConferenceManagement.Business.AdminDataAccess;
+using ConferenceManagement.Infrastructure.Commands.AdminCommands;
+using ConferenceManagement.Model;
+using MediatR;
+
+namespace ConferenceManagement.Handlers.AdminHandlers
+{
+    public class AddRoomCommandHandler : IRequestHandler<AddRoomCommand, bool>
+    {
+        private readonly IAdminDataAccess _adminDataAccess;
+
+        public AddRoomCommandHandler(IAdminDataAccess adminDataAccess)
+        {
+            _adminDataAccess = adminDataAccess;
+        }
+
+        public async Task<bool> Handle(AddRoomCommand request, CancellationToken cancellationToken)
+        {
+            ConferenceRoom conferenceRoom = new ConferenceRoom()
+            {
+                RoomName = request.RoomName,
+                Capacity = request.Capacity,
+                IsAVRoom = request.IsAVRoom,
+                Image = request.Image,
+            };
+
+            return await _adminDataAccess.AddRoom(conferenceRoom);
+        }
+    }
+}
