61d5a6d3d70f39d5d395cf8c7bfe80a24ae8a26c
Ashish Sahu <ashsahu9713@gmail.com>
2023-08-01 13:00:30 +0530

    Update

---
+++ ConferenceManagement/Model/Notification.cs
@@ -0,0 +1,11 @@
+﻿namespace ConferenceManagement.Model
+{
+    public class Notification
+    {
+        public int NotificationId { get; set; }
+        public string NotificationData { get; set; }
+        public int UserId { get; set; }
+       
+
+    }
+}
